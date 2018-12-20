a7b97164e2e31e3d880efe516c7cd77978041b6b
WEBER Antoine <ghostsmaker@hotmail.com>
2018-12-20 23:54:49 +0100

    modif css

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf node_modules
 docker-compose run app npm install
-RUN npm rebuild node-sass --force
+docker-compose run app npm rebuild node-sass --force
 docker-compose run app ionic cordova build android
 cp ./platforms/android/app/build/outputs/apk/debug/app-debug.apk ./
