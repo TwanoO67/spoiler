0491faebdc0043f12566185a356aa646ed3821f4
Antoine Weber <aweber@numesia.fr>
2019-01-18 14:20:39 +0100

    fix ordre des ecrans

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -12,7 +12,7 @@ export class GamePage {
   public reponse = "";
   public total = 0;
   public tentative = 0;
-  public max_tentative = 3;
+  public max_tentative = 2;
   public taille_partie = 10;
   public default_affiche = 'https://s3.amazonaws.com/storenvy/product_photos/112446/spoiler_alert_original.jpg';// 'http://i.imgrpost.com/imgr/2017/07/29/bond4.jpg';
   public past = 0;
@@ -85,14 +85,31 @@ export class GamePage {
     if(verify){
       this.step = "good_answer";
       this.total++;
+      if(this.spoiler){
+        this.spoiler.done = true;
+        this.done.push(this.spoiler);
+        //on remplace l'affiche si existante
+        if(!this.spoiler.id_themoviedb ){
+          this.affiche = this.default_affiche;
+        }
+        else {
+          this.http.get("https://api.themoviedb.org/3/movie/"+this.spoiler.id_themoviedb+"?api_key="+this.apikey).subscribe(
+            (reponse: any) => {
+              console.log(reponse);
+              this.affiche = 'https://image.tmdb.org/t/p/w500'+ reponse.poster_path;
+          });
+        }
+      }
+      else{
+        this.affiche = this.default_affiche;
+      }
     }
     else{
       this.step = "wrong_answer";
-      this.checkTentative();
     }
   }
 
-  private checkTentative(){
+  public checkTentative(){
     if(this.tentative === this.max_tentative){
       this.nextSpoiler()
     }
@@ -114,26 +131,7 @@ export class GamePage {
   private nextSpoiler(){
     //on marque le spoiler comme vu
     this.step = "question";
-    if(this.spoiler){
-      this.spoiler.done = true;
-      this.done.push(this.spoiler);
-      //on remplace l'affiche si existante
-      if(!this.spoiler.id_themoviedb ){
-        this.affiche = this.default_affiche;
-      }
-      else {
-        this.http.get("https://api.themoviedb.org/3/movie/"+this.spoiler.id_themoviedb+"?api_key="+this.apikey).subscribe(
-          (reponse: any) => {
-            console.log(reponse);
-            this.affiche = 'https://image.tmdb.org/t/p/w500'+ reponse.poster_path;
-        });
-      }
-    }
-    else{
-      this.affiche = this.default_affiche;
-    }
-
-
+    
     this.available = this.donnee.filter((spoil) => {
       return !spoil.done;
     });
