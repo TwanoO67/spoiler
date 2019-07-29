997dd1b11431813ff0867e39e7cae84d28da286c
bilowww <romain.bihl@gmail.com>
2019-07-29 22:42:04 +0200

    install ionic

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf node_modules
-docker-compose run app npm install
-docker-compose run app npm rebuild node-sass --force
-docker-compose run app ionic cordova build android
+docker-compose run --rm app npm install
+docker-compose run --rm app npm rebuild node-sass --force
+docker-compose run --rm app ionic cordova build android
 cp ./platforms/android/app/build/outputs/apk/debug/app-debug.apk ./
