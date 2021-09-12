86d40b6deb155e54e85484533513fb7b8b035359
WEBER Antoine <ghostsmaker@hotmail.com>
2021-09-12 08:40:20 +0200

    import bo crud

--- app/build.sh
+++ app/build.sh
@@ -3,4 +3,4 @@ rm -rf node_modules
 docker-compose run app npm install
 docker-compose run app npm rebuild node-sass --force
 docker-compose run app ionic cordova build android
-cp ./platforms/android/app/build/outputs/apk/debug/app-debug.apk ./
+cp ./platforms/android/app/build/outputs/apk/debug/app-debug.apk ../
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  urlapi: "https://test.weberantoine.fr/SPOILER/api.php/"
-};
